552dd9b838bc7322df260ecabbb388e434747afc
Aaron Zuspan <aa.zuspan@gmail.com>
2022-01-23 18:36:53 -0800

    Make notifypy a required import

--- taskee/notifiers/native.py
+++ taskee/notifiers/native.py
@@ -1,17 +1,11 @@
+import notifypy
+
 from taskee.notifiers.notifier import Notifier
 
 
 class Native(Notifier):
-    def __init__(self):
-        try:
-            import notifypy
-
-            self.notifypy = notifypy
-        except ImportError:
-            raise ImportError("")
-
     def send(self, title, message):
-        notification = self.notifypy.Notify()
+        notification = notifypy.Notify()
         notification.application_name = "taskee"
         notification.title = title
         notification.message = message
