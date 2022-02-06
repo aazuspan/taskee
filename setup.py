d3fb8bb25da5ad545fe27f2b0976ed6375631e16
azuspan607 <aa.zuspan@gmail.com>
2022-02-05 23:55:55 -0800

    Bump 0.0.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup  # type: ignore
 
-version = "0.0.2"
+version = "0.0.3"
 
 requirements = ["earthengine-api", "rich", "humanize", "notify-py", "requests", "click"]
 test_requirements = ["pytest"]
